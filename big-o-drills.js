fb44e67cecc01a845a8b0d36b41d707c5dd1846c
Sonya Slegers <sonjay08@gmail.com>
2018-07-24 15:10:48 -0500

    finished iterative questions

--- big-o-drills.js
+++ big-o-drills.js
@@ -79,7 +79,7 @@ function createPairs(arr) {
 // the following items, then the second item paired with each of the other items, etc for 
 // all items in the array
 
-// Polynomial time or O(^2) because the algorithm puts the input through two levels of loops
+// Polynomial time or O(n^2) because the algorithm puts the input through two levels of loops
 // before it finishes computing the output
 
 //Computing fibonaccis
@@ -184,7 +184,388 @@ function isPrime(n) {
 // Linear time of O(n) because the algorithm must iterate through two consecutive loops
 // to determine the output, the larger the value of 'n', the longer the run time will be
 
+////////////////////////////////// Recursive Big O /////////////////////////////////////////
 
+// Counting Sheep
+
+function countSheep(num){
+  //stopping condition of base case
+  if(num === 0){
+      console.log(`All sheep jumped over the fence`);
+  } 
+  //this is the recursive case
+  //this will be executed until it reaches base case
+  else{
+      console.log(`${num}: Another sheep jump over the fence`);
+      countSheep(num-1);
+  }
+}
+
+// Linear Time or O(n)
+
+
+// Array Double
+
+function double_all(arr) {
+  if (!arr.length) {
+      return [];
+  }
+  return [arr[0] * 2, ...double_all(arr.slice(1))];
+}
+
+console.log(double_all([1, 2, 3,5, 6]));
+
+// Linear Time or O(n)
+
+
+//Reverse String
+
+function reverseString(str) {
+  if (str.length < 2) {
+      return str;
+  }
+  return reverseString(str.slice(1)) + str[0];
+}
+
+// Linear Time or O(n)
+
+
+//Triangular number
+
+function triangle(n) {
+  if (n < 2) 
+      return n;
+  return n + triangle(n - 1);
+}
+
+// Linear Time or 0(n)
+
+
+// String Splitter
+
+function split(str, sep) {
+  var idx = str.indexOf(sep);
+  if (idx == -1) 
+      return [str];
+  //you don't have to return an array, you can return a string as an array of 
+  //character 
+  //return str;
+  return [str.slice(0, idx)].concat(split(str.slice(idx + sep.length), sep))
+//all these are valid syntax
+//return (str.slice(0,idx) + (split(str.slice(idx + sep.length), sep)))
+//return str.slice(0,idx).concat(split(str.slice(idx + sep.length), sep))
+}
+
+//Linear Time O(n)
+
+
+//Binary Representation
+
+function convertToBinary(num){
+  if(num>0){
+      let binary = Math.floor(num%2); //save the reminder in binary
+  //divide the number by 2 and send that to the function again
+  //carry the reminder to the next recursion call
+      return (convertToBinary(Math.floor(num/2))+ binary);
+  }else{
+      return ''; //base case - at some point the divisions will lead to 0
+  }
+}
+
+// Logarithmic time or O(logn)
+
+
+//Factorial 
+
+function factorial(n) {  
+  // Base Case - when n is equal to 0, we stop the recursion
+  if (n === 0) {
+    return 1;
+  }
+  // This is our Recursive Case
+  // It will run for all other conditions except when n is equal to 0
+  return n * factorial(n - 1);
+}
+
+// Linear Time or O(n)
+
+
+//Fibonacci
+
+function fibonacci(n) {
+  // Base case
+  if (n <= 0) {
+    return 0;
+  }
+  // Base case
+  if (n <= 2) {
+    return 1;
+  }	
+  // Recursive case
+  return fibonacci(n - 1) + fibonacci(n - 2);	
+}
+
+// Exponential Time or O(2^n) 
+
+
+//Anagrams
+
+function anagrams(prefix, str){
+  if(str.length <= 1){
+      console.log(`The anagram is ${prefix}${str}`);
+  } else {
+      for(let i=0; i<str.length; i++){
+          let currentLetter = str.substring(i, i+1); 
+          let previousLetters = str.substring(0,i);
+          let afterLetters = str.substring(i+1);
+          anagrams(prefix+currentLetter, previousLetters+afterLetters);
+      }
+  }
+}
+function printAnagram(word){
+  //console.log(`The word for which we will find an anagram is ${word}`);
+  anagrams(' ', word);
+
+}
+
+// Polynomial time or O(n^2)
+
+
+//Animal Hierarchy
+
+const animalHierarchy = [
+  {id: 'Animals', parent: null},
+  {id: 'Mammals', parent: 'Animals'},
+  {id: 'Dogs', parent:'Mammals' },
+  {id: 'Cats', parent:'Mammals' },
+  {id: 'Golden Retriever', parent: 'Dogs'},
+  {id: 'Husky', parent:'Dogs' },
+  {id: 'Bengal', parent:'Cats' }
+]
+
+// ==============================
+function traverse(animalHierarchy, parent) {
+  let node = {};
+  animalHierarchy.filter(item => item.parent === parent)
+                 .forEach(item => node[item.id] = traverse(animalHierarchy, item.id));
+  return node;  
+}
+
+console.log(traverse(animalHierarchy, 'Mammals'));
+
+// Polynomial Time or O(n^3)
+
+
+//Organization
+
+let organization = {
+	"Zuckerberg": {		
+		"Schroepfer": {
+			"Bosworth": {
+				"Steve":{},
+				"Kyle":{},
+				"Andra":{}
+			},
+			"Zhao": {
+				"Richie":{},
+				"Sofia":{},
+				"Jen":{}
+			},
+			"Badros": {
+				"John":{},
+				"Mike":{},
+				"Pat":{}
+			},
+			"Parikh": {
+				"Zach":{},
+				"Ryan":{},
+				"Tes":{}
+			}
+		},
+		"Schrage": {
+			"VanDyck": {
+				"Sabrina":{},
+				"Michelle":{},
+				"Josh":{}
+			},
+			"Swain": {
+				"Blanch":{},
+				"Tom":{},
+				"Joe":{}
+			},
+			"Frankovsky": {
+				"Jasee":{},
+				"Brian":{},
+				"Margaret":{}
+			}
+		},
+		"Sandberg": {
+			"Goler": {
+				"Eddie":{},
+				"Julie":{},
+				"Annie":{}
+			},
+			"Hernandez": {
+				"Rowi":{},
+				"Inga":{},
+				"Morgan":{}
+			},
+			"Moissinac": {
+				"Amy":{},
+				"Chuck":{},
+				"Vinni":{}
+			},
+			"Kelley": {
+				"Eric":{},
+				"Ana":{},
+				"Wes":{}
+			}
+}}};
+
+function traverseA(data, depth = 0) {
+	let indent = " ".repeat(depth * 4);
+	Object.keys(data).forEach(key => {
+		console.log(indent + key);
+		traverseA(data[key], depth + 1)
+	});
+}
+
+function traverseB(node, indent=0) {
+	for (var key in node) {
+		console.log(" ".repeat(indent), key);
+		traverseB(node[key], indent + 4);
+	}
+}
+
+// Linear Time O(n)
+
+
+//////////////////////////// Iterative ////////////////////////////////
+
+//Counting Sheep
+
+function countSheepLoop(num){
+  for(let i=num; i>0; i--){
+      console.log(`counting sheeps ${i}`);
+  }
+}
+countSheepLoop(10);
+
+// O(n)
+
+// Double Array 
+
+function double_all(arr) {
+  var ret = Array(arr.length);
+  for (var i = 0; i < arr.length; ++i) {
+      ret[i] = arr[i] * 2;
+  }
+  return ret;
+}
+let arr = [10,4,5,2,1];
+console.log(double_all(arr));
+
+// O(n)
+
+//Reverse String
+
+function reverse_tail(str) {
+  var accumulator = "";
+  while (str !== "") {
+    accumulator = str[0] + accumulator;
+    str = str.slice(1);
+  }
+  return accumulator;
+}
+
+// O(n)
+
+// Nth Triangular Number
+
+function triangle(n) {
+  var tot = 0;
+  for (var i = 1; i <= n; ++i) {
+    tot += n;
+  }
+  return tot;
+}
+
+// O(n)
+
+// String Splitter
+
+function split(str, sep) {
+  var ret = [];
+  while (true) {
+      var idx = str.indexOf(sep);
+      if (idx == -1) break;
+ret.push(str.slice(0, idx))
+str = str.slice(idx + sep.length);
+  }
+  ret.push(str);
+  return ret;
+}
+
+// O(n)
+
+// Binary Representation
+
+function convertToBinaryIter(num){
+  var binary = '';
+  while(num>0){
+      let rem = Math.floor(num%2);
+      binary = rem + binary;
+      num = Math.floor(num/2);
+  }
+  return binary;
+
+}
+
+// O(log(n))
+
+// Factorial
+
+function factorialIterative(number){
+  let fact = 1;
+  for (let i = 1; i <= number; i++){
+      fact *= i;
+  }
+  return fact;
+}
+console.log(factorialIterative(5));
+
+// O(n)
+
+// Fibonnacci
+
+function fibonacciIterative(number){
+  let num1 = 1;
+  let num2 = 0;
+  let fib = null;
+  while(number > 0){
+      fib = num1;
+      num1 = num1+num2;
+      num2 = fib;
+      number--;
+  }
+  return num2;
+
+}
+
+// O(log(n)) or O(n) ??
+
+// or //
+
+function fibonacciIterative2(number){
+  let [num1, num2] = [1,0];
+  while(number-- > 0){
+      [num1, num2] = [num2+num1, num1]
+  }
+  return num2;
+
+}
+
+// O(log(n)) or O(n) ??
 
 
 
